ec48ee3701a0cdc26f872ba9bdb8e6d5219abfba
Schott Liam <liam.schott@etu.umontpellier.fr>
2024-01-07 23:54:45 +0100

    debut site

---
+++ src/accueil.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Présentation</title>
+    <link href="../ressources/style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div>
+        <div id="burger">
+            <a href="" class="submenu">Pages:</a>
+
+            <div id="b_submenu">
+                <a href="./accueil.php"
+                   class="b_subpages">Presentation</a>
+                <a href="./cv.php"
+                   class="b_subpages">CV</a>
+                <a href="./projets.php"
+                   class="b_subpages">Réalisation</a>
+            </div>
+        </div>
+        <h1>Liam Schott</h1>
+    </div>
+    <p><b>Présentation</b></p>
+</header>
+<div class="container">
+    <h2>Bienvenue</h2>
+    <p>
+        Je suis passionné par l'informatique depuis le primaire. Je me lançais des défis préférais les jeux d’énigmes à d’autres,
+        et je continue à ce jour à me régaler dans la réalisation de  projets et la résolution de  problèmes.
+        Cette opportunité d’être à l'IUT de Montpellier me permet de travailler avec passion.
+    </p>
+    <p>
+        De plus, j’étais au lycée en sport étude en natation et j’ai réussit à participer à un championnat de France. Nager, Voyager, lire et
+        même dessiner  occupe le peu de temps qui me reste reste, sans oublier les jeux de sociétés en famille.
+    </p>
+    <p>
+        En deuxième année de BUT en informatique je bénéficie d’une excellente formation. Cette expérience académique
+        et me donne les bases pour continuer à progresser dans les domaines comme: la communication, la gestion de projet, le développement d'applications.
+    </p>
+
+    <h2>Expérience</h2>
+    <p>
+        A l’ occasion du stage scolaire de 3<sup>e</sup> lors de mes 14 ans,  j’ai eu le privilège d’effectuer une immersion au cœur d’un service informatique DSI Pôle Emploi,
+        et j’ai côtoyé des développeurs dans leur environnement. Cette expérience a été décisive dans le choix de mon parcours d’étudiant.
+    </p>
+    <p>
+        Je recherche activement l’entreprise de programmation ou je pourrais effectuer mon contrat d’alternance prévu dans mon cursus.
+    </p>
+    <p>
+        J’ai aussi une petite expérience professionnelle en job d’été à Carrefour, cela m’a mis dans le bain de la vie salariale, employé au drive.
+    </p>
+
+</div>
+
+</body>
+</html>
---
+++ src/cv.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Curriculum Vitae</title>
+    <link href="../ressources/style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div>
+        <div id="burger">
+            <a href="" class="submenu">Pages:</a>
+
+            <div id="b_submenu">
+                <a href="./accueil.php"
+                   class="b_subpages">Presentation</a>
+                <a href="./cv.php"
+                   class="b_subpages">CV</a>
+                <a href="./projets.php"
+                   class="b_subpages">Réalisation</a>
+            </div>
+        </div>
+        <h1>Mathys Capo</h1>
+    </div>
+    <p>Curriculum Vitae</p>
+</header>
+<div class="container">
+    <img src="../ressources/images/cv.png" alt="image du CV" id="imageCV"/>
+    <div class="lien">
+        <a href="www.linkedin.com/in/liam-schott-741b2b263" id="lkd">
+            <img src="../ressources/images/linkedin.png" alt="lkd" class="imgfooter">
+        </a>
+        <a href="https://github.com/Mathys-Capo">
+            <img src="../ressources/images/github.png" alt="github" class="imgfooter">
+        </a>
+    </div>
+</div>
+</body>
+</html>
