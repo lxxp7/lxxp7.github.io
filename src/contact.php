0e958c8b6009261939cf98358e031b03f875023e
Schott Liam <liam.schott@etu.umontpellier.fr>
2024-01-08 02:46:24 +0100

    page contact

---
+++ src/contact.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="../ressources/style.css">
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+<header>
+    <nav class="containerH">
+        <div class="parent-link">
+            <a href="./accueil.php" class="social-links">Présentation</a>
+            <a href="./projets.php" class="social-links">Projets</a>
+            <a href="./cv.php" class="social-links">CV</a>
+            <a href="./contact.php" class="social-links">Contact</a>
+        </div>
+
+    </nav>
+
+    <div class="background">
+    <div class="container">
+        <div class="screen">
+            <div class="screen-header">
+
+                <div class="screen-header-right">
+                    <div class="screen-header-ellipsis"></div>
+                    <div class="screen-header-ellipsis"></div>
+                    <div class="screen-header-ellipsis"></div>
+                </div>
+            </div>
+            <div class="screen-body">
+                <div class="screen-body-item left">
+                    <div class="app-title">
+                        <span>CONTACTEZ -</span>
+                        <span>MOI</span>
+                    </div>
+                </div>
+                <div class="screen-body-item">
+                    <div class="app-form">
+                        <div class="app-form-group">
+                            <input class="app-form-control" placeholder="NOM">
+                        </div>
+                        <div class="app-form-group">
+                            <input class="app-form-control" placeholder="EMAIL">
+                        </div>
+                        <div class="app-form-group message">
+                            <input class="app-form-control" placeholder="MESSAGE">
+                        </div>
+                        <div class="app-form-group buttons">
+                            <button class="app-form-button" onclick="sendEmail()">ENVOYER</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</header>
+<footer>
+    <div class="name">Liam Schott</div>
+    <div class="icon">
+
+        <a href="https://www.linkedin.com/in/liam-schott-741b2b263/" target="_blank" class="sm-icon">
+            <ion-icon name="logo-linkedin"></ion-icon>
+        </a>
+        <a href="https://github.com/lxxp7/" target="_blank" class="sm-icon">
+            <ion-icon name="logo-github"></ion-icon>
+        </a>
+
+    </div>
+</footer>
+</body>
+</html>
+
+<script>
+    function sendEmail() {
+        var name = document.querySelector('.app-form-control[placeholder="NAME"]').value;
+        var email = document.querySelector('.app-form-control[placeholder="EMAIL"]').value;
+        var message = document.querySelector('.app-form-control[placeholder="MESSAGE"]').value;
+
+        var subject = "Portfolio Contact Form Submission";
+        var mailtoLink = "mailto:lixmp7@gmail.com" +
+            "?subject=" + encodeURIComponent(subject) +
+            "&body=" + encodeURIComponent("Name: " + name + "\nEmail: " + email + "\nMessage: " + message);
+
+        window.location.href = mailtoLink;
+    }
+</script>
--- src/cv.php
+++ src/cv.php
@@ -17,6 +17,7 @@
             <a href="./accueil.php" class="social-links">Présentation</a>
             <a href="./projets.php" class="social-links">Projets</a>
             <a href="./cv.php" class="social-links">CV</a>
+            <a href="./contact.php" class="social-links">Contact</a>
         </div>
     </nav>
 
